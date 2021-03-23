420de6eaf903c405c43312b0f119815753346b69
Mark Lin <61925580+Marklin2289@users.noreply.github.com>
2021-03-22 19:58:35 -0700

    finished API

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,20 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 import SearchBar from "./SearchBar";
 
 const App = () => {
-  const onSearchSubmit = (term) => {
-    console.log(term);
+  const [images, setImages] = useState([]);
+  const onSearchSubmit = async (term) => {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: "Client-ID o-c6Bl7Ka3ooKK3hAGlIYlAn3nE0Sw5c8c_dKHhqoF0",
+      },
+    });
+    //   .then((response) => {
+    //     console.log(response.data.results);
+    //   });
+    console.log(response.data.results);
   };
   return (
     <div className="ui container" style={{ marginTop: "10px" }}>
